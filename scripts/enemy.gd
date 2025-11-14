cfb2aa12160144b7a486a8426feae7e6fb8ae44e
unkowndevil5656 <uziabdul56@gmail.com>
2025-11-14 20:26:21 +0400

    auooo

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -8,3 +8,23 @@ var current_health: int
 
 @onready var name_label = $NameLabel
 @onready var health_label = $HealthLabel
+
+
+func _ready():
+	current_health = max_health
+	name_label.text = enemy_name
+	update_health_display()
+	
+	
+func take_damage(amount: int):
+	current_health -= amount
+	update_health_display()
+	if current_health <= 0:
+		die()
+	
+func update_health_display():
+	health_label.text = "HP:" + str(current_health)
+	
+func die():
+	queue_free()
+	print(enemy_name + "defeated!!")
